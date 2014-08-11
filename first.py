ff5efaef594ff4b553eb981b24ee79447c7eb61a
joeknows718 <joeknows718@gmail.com>
2014-08-11 16:44:53 -0400

    linking form to db

--- first.py
+++ first.py
@@ -27,7 +27,7 @@ class Role(db.Model):
 	__tablename__= 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
-	users = db.relationship('User', backref='role')
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
@@ -56,13 +56,17 @@ def index():
 	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username = name.form.data)
+			db.session.add(user)
+			session['known'] = False 
+		else:
+			session['known'] = True 
 		session['name'] = form.name.data
-		form.name.data = ''
+		form.name.data =  ''
 		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+	return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow(), known = session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
